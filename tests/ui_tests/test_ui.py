0c91a1f8471ec2663bbb5131a2ffad4ee425037c
Maxim <maxim.pyshynskiy@gmail.com>
2024-12-07 23:22:31 +0100

    proj init

--- tests/ui_tests/test_ui.py
+++ tests/ui_tests/test_ui.py
@@ -1,7 +1,6 @@
-import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
