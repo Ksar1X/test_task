8bac6334eb0ef717935ac4ceccc4c2170abc352b
Maxim <maxim.pyshynskiy@gmail.com>
2024-12-16 23:56:05 +0100

    proj init

---
+++ page_objects/singup_page.py
@@ -0,0 +1,40 @@
+from page_objects.base_page import BasePage
+
+
+class SingUpPage(BasePage):
+
+    FIRST_NAME_FIELD = ("xpath", "//input[@id='firstName']")
+    LAST_NAME_FIELD = ("xpath", "//input[@id='lastName']")
+    EMAIL_FIELD = ("xpath", "//input[@id='email']")
+    PASSWORD_FIELD = ("xpath", "//input[@id='password']")
+
+    SUBMIT_BUTTON = ("xpath", "//button[@id='submit']")
+    CANCEL_BUTTON = ("xpath", "//button[@id='cancel']")
+
+    ERROR = ("xpath", "//span[@id='error']")
+
+
+    def open_browser(self):
+        self.driver.get(self.sing_up_url)
+
+    def click_on_submit_button(self):
+        self.driver.find_element(*self.SUBMIT_BUTTON).click()
+
+    def create_user(self, email, password, first_name, last_name):
+        self.open_browser()
+        first_name_field = self.driver.find_element(*self.FIRST_NAME_FIELD)
+        first_name_field.send_keys(first_name)
+        last_name_field = self.driver.find_element(*self.LAST_NAME_FIELD)
+        last_name_field.send_keys(last_name)
+        email_field = self.driver.find_element(*self.EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_field = self.driver.find_element(*self.PASSWORD_FIELD)
+        password_field.send_keys(password)
+        self.click_on_submit_button()
+
+    def click_on_cancel_button(self):
+        self.driver.get(self.sing_up_url)
+        self.driver.find_element(*self.CANCEL_BUTTON).click()
+
+    def find_error(self):
+        return self.driver.find_element(*self.ERROR)
--- tests/ui_tests/test_ui_login_page.py
+++ tests/ui_tests/test_ui_login_page.py
@@ -1,64 +1,37 @@
-from page_objects.base_page import BasePage
 from selenium.webdriver.support import expected_conditions as EC
+from tests.test_base import BaseTest
 
 
-class TestUILoginPage(BasePage):
-
-    SUBMIT_BUTTON = ("xpath", "//button[@id='submit']")
-    SIGN_UP_BUTTON = ("xpath", "//button[@id='signup']")
-    EMAIL_FIELD = ("xpath", "//input[@id='email']")
-    PASSWORD_FIELD = ("xpath", "//input[@id='password']")
-    ERROR = ("xpath", "//span[@id='error']")
+class TestUILoginPage(BaseTest):
 
 
     def test_user_cannot_login_with_empty_fields(self):
-        self.driver.get(self.base_url)
-        button = self.driver.find_element(*self.SUBMIT_BUTTON)
-        button.click()
-        error = self.driver.find_element(*self.ERROR)
+        self.login_page.login(email='', password='')
+        error = self.login_page.find_error()
         assert error is not None
 
     def test_cannot_login_unregistered_user(self):
-        self.driver.get(self.base_url)
-        email_field = self.driver.find_element(*self.EMAIL_FIELD)
-        email_field.send_keys("auoifhqw@g.com")
-        password_field = self.driver.find_element(*self.PASSWORD_FIELD)
-        password_field.send_keys("joiafsajs")
-        button = self.driver.find_element(*self.SUBMIT_BUTTON)
-        button.click()
-        error = self.driver.find_element(*self.ERROR)
+        user = self.random_user.generate()
+        self.login_page.login(email=user.email, password=user.password)
+        error = self.login_page.find_error()
         assert error is not None
 
     def test_cannot_login_without_email_field(self):
-        self.driver.get(self.base_url)
-        password_field = self.driver.find_element(*self.PASSWORD_FIELD)
-        password_field.send_keys("joiafsajs")
-        button = self.driver.find_element(*self.SUBMIT_BUTTON)
-        button.click()
-        error = self.driver.find_element(*self.ERROR)
+        user = self.random_user.generate()
+        self.login_page.login(email='', password=user.password)
+        error = self.login_page.find_error()
         assert error is not None
 
     def test_cannot_login_without_password_field(self):
-        self.driver.get(self.base_url)
-        email_field = self.driver.find_element(*self.EMAIL_FIELD)
-        email_field.send_keys("auoifhqw@g.com")
-        button = self.driver.find_element(*self.SUBMIT_BUTTON)
-        button.click()
-        error = self.driver.find_element(*self.ERROR)
+        user = self.random_user.generate()
+        self.login_page.login(email=user.email, password='')
+        error = self.login_page.find_error()
         assert error is not None
 
     def test_sign_up_button(self):
-        self.driver.get(self.base_url)
-        button = self.driver.find_element(*self.SIGN_UP_BUTTON)
-        button.click()
-        assert button is not None
+        self.login_page.click_on_singup_button()
+        assert self.login_page.wait.until(EC.url_changes(self.login_page.contact_url))
 
     def test_login_user(self):
-        self.driver.get(self.base_url)
-        email_field = self.driver.find_element(*self.EMAIL_FIELD)
-        email_field.send_keys("garynychxxx@gmail.com")
-        password_field = self.driver.find_element(*self.PASSWORD_FIELD)
-        password_field.send_keys("raketa123")
-        button = self.driver.find_element(*self.SUBMIT_BUTTON)
-        button.click()
-        assert self.wait.until(EC.url_changes(self.contact_url))
+        self.login_page.login(email="garynychxxx@gmail.com", password="raketa123")
+        assert self.login_page.wait.until(EC.url_changes(self.login_page.contact_url))
